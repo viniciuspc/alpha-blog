94b3f3208608d094040cdf09bceb8e6adeb4cfaf
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-10 15:38:57 +0100

    Add listing articles in index article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,8 @@ class ArticlesController < ApplicationController
     # @ will convert the variable to be an instance variable
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
 end
