6e4ba8b45f7f2bb431bc26b88eafe1881bb4e95f
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-11 10:04:00 +0100

    Save new article to the database and redirect to show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,19 @@ class ArticlesController < ApplicationController
 
   # Save the new article in the databbase
   def create
-    render plain: params[:article]
+    # Require the top level atribute article and permit only the title and Description
+    # from it.
+    # It is a security feature included at rails 4
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+
+    # Specify where the browser should go after the article is saved
+    # Rails will extract the id from @article and redirect to the show path.
+    # Since the show path is /articles/:id
+
+    # redirect_to article_path(@article)
+
+    # Shortened the previous redirect_to
+    redirect_to @article
   end
 end
