18a3fbf0136cb875329817a965e735877bd288b5
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-15 15:45:32 +0100

    Add create and delete functions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      # flash.now will not wait for another http request to display,
+      # this happens because render does not make other http request it only
+      # rerender the page
+      flash.now[:alert] = "There was something wrong with your login details"
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
